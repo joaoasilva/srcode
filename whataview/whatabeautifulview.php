27c1ed875dbf207df2c8dc63636047c88ca2ac0b
João Silva <joaoasilva@gmail.com>
2013-01-21 11:39:40 +0000

    html error

--- whataview/whatabeautifulview.php
+++ whataview/whatabeautifulview.php
@@ -1,6 +1,6 @@
     <div class="container-fluid">
 
-      <div id="menu" class="jumbotron data-spy="affix"">
+      <div id="menu" class="jumbotron" data-spy="affix">
         <p class="skype_btn">
           <script type="text/javascript" src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"></script>
           <a href="skype:<?=$skype_id;?>?add"><img src="http://download.skype.com/share/skypebuttons/buttons/add_blue_white_195x63.png" style="border: none;" width="195" height="63" alt="Add me to Skype" /></a>
